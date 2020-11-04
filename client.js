febc72dd446f15639ee08b6b4534f1401d53ecaa
Clarence Chiu <clarchiu@gmail.com>
2020-11-04 20:28:59 +0000

    added write to server on connection

--- client.js
+++ client.js
@@ -11,6 +11,11 @@ const connect = function() {
   // interpret incoming data as text
   conn.setEncoding('utf8'); 
 
+  conn.on('connect', () => {
+    console.log("connected to server");
+    conn.write("Name: CC");
+  });
+
   conn.on('data', (data) => {
     console.log(data);
   })
